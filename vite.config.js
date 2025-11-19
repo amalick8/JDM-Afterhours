17dd1fb69bddec2d8fac7e51440028bae2ade7c1
Ammar Malick <ammarmalick2006@gmail.com>
2025-11-19 04:17:32 -0600

    1

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // Github Pages base path
+  base: "/JDM-Afterhours/",
+
+  // Fix dev server so routing works normally during local dev
+  server: {
+    host: true,
+  },
 });
